ef6b0e659e1f88c4abf243fd0bb1b70ae0828813
Muhammad Naveed <muhammadnaveedgurmanii@gmail.com>
2026-08-21 16:54:19 +0500

    Remove duplicate Vercel API function files

--- api/[...path].js
+++
@@ -1,9 +0,0 @@
-let appPromise;
-
-module.exports = async (request, response) => {
-  appPromise ??= import("../artifacts/api-server/dist/app.mjs").then(
-    (module) => module.default,
-  );
-  const app = await appPromise;
-  return app(request, response);
-};
